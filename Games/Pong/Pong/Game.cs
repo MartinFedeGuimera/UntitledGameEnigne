a4f1c18224e622af7b3ee37e13412aa9fcbde054
Martin Guimera <martinfedeguimera2009@gmail.com>
2026-03-25 21:41:07 -0300

    Window Creation
    Created a Window and detected when Esc pressed to close it

---
+++ Games/Pong/Pong/Game.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Pong
+{
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() 
+            { 
+                ClientSize = (width, height),
+                Title = title 
+            }) 
+        { }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            if(KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+    }
+}
--- Games/Pong/Pong/Program.cs
+++ Games/Pong/Pong/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Pong
+﻿using System;
+
+
+namespace Pong
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            using (Game pong = new Game(800, 600, "Pong"))
+            {
+                pong.Run();
+            }
         }
     }
 }
