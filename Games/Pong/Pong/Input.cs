9488a3367eec55f58bc53c86c3c4a99588bbbb71
Martin Guimera <martinfedeguimera2009@gmail.com>
2026-08-29 16:56:56 -0300

    Input Basic System Implemented
    Now you can call Input class from every script and check if a key is down, is pressed or was just released

--- Games/Pong/Pong/Input.cs
+++ Games/Pong/Pong/Input.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace Pong
 {
-    internal class Input
+    internal static class Input
     {
+        private static KeyboardState currentState;
+        private static KeyboardState previousState;
+
+        public static void Update(KeyboardState keyboardState)
+        {
+            previousState = currentState;
+            currentState = keyboardState;
+        }
+
+        public static bool IsKeyDown(Keys key) => currentState.IsKeyDown(key);
+        public static bool IsKeyPressed(Keys key) => currentState.IsKeyDown(key) && !previousState.IsKeyDown(key);
+        public static bool IsKeyReleased(Keys key) => !currentState.IsKeyDown(key) && previousState.IsKeyDown(key);
     }
 }
