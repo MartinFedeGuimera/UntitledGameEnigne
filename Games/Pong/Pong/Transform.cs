44ba19395b4597cb896551516236c0fd18332d46
Martin Guimera <martinfedeguimera2009@gmail.com>
2026-08-29 16:06:24 -0300

    Transform Class Done!

---
+++ Games/Pong/Pong/Transform.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace Pong
+{
+    internal class Transform
+    {
+        private Vector3 position;
+        private Vector3 rotation;
+        private Vector3 scale;
+
+        public Vector3 Position { get { return position; }
+            set { position = value; } }
+        public Vector3 Rotation { get { return rotation; } set { rotation = value; } }
+        public Vector3 Scale { get { return scale; } set { scale = value; } }
+    }
+}
