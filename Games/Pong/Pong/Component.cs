d08687cea6f8284f99f28eea067028b96c219778
Martin Guimera <martinfedeguimera2009@gmail.com>
2026-08-29 18:45:29 -0300

    Created Component class
    A basic Component class created and implemented in GameObject.cs

---
+++ Games/Pong/Pong/Component.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong
+{
+    internal abstract class Component
+    {
+        public string Name { get; set; }
+        public GameObject GameObect {  get; internal set; }
+
+        public virtual void Start() { }
+
+        public virtual void Update(float deltaTime) { }
+    }
+}
--- Games/Pong/Pong/GameObject.cs
+++ Games/Pong/Pong/GameObject.cs
@@ -5,14 +5,51 @@
         public string Name;
         private Transform Transform;
 
+        private List<Component> components = new List<Component>();
+
         public GameObject(string name)
         {
             Name = name;
             Transform = new Transform();
         }
 
-        public void Start() { }
+        public T AddComponent<T>() where T : Component, new()
+        {
+            T component = new T();
+            component.GameObect = this;
 
-        public void Update(float deltaTime) { }
+            components.Add(component);
+
+            return component;
+        }
+
+        public T GetComponent<T>() where T : Component, new()
+        {
+            foreach (Component component in components)
+            {
+                if (component is T)
+                {
+                    return (T)component;
+                }
+            }
+
+            return null;
+        }
+
+        public void Start() 
+        { 
+            foreach(Component component in components)
+            {
+                component.Start();
+            }
+        }
+
+        public void Update(float deltaTime) 
+        {
+            foreach(Component component in components)
+            {
+                component.Update(deltaTime);
+            }
+        }
     }
 }
