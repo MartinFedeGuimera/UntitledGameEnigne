a567b54c4569c70b9539b3eac4848c6d468ef418
Martin Guimera <martinfedeguimera2009@gmail.com>
2026-08-30 00:25:43 -0300

    Created Mesh class
    Now vertices and indices are saved in a Mesh class

---
+++ Games/Pong/Pong/Mesh.cs
@@ -0,0 +1,55 @@
+﻿using OpenTK.Graphics.OpenGL4;
+
+namespace Pong
+{
+    internal class Mesh
+    {
+        private int VertexArrayObject {  get; set; }
+
+        private int VertexBufferObject { get; set; }
+
+        private int ElementBufferObject { get; set; }
+
+        private float[] vertexs;
+
+        private uint[] indexes;
+
+        public Mesh(float[] vertexs, uint[] indexes)
+        {
+            this.vertexs = vertexs;
+            this.indexes = indexes;
+        }
+
+        public void Initialize()
+        {
+            VertexArrayObject = GL.GenVertexArray();
+            VertexBufferObject = GL.GenBuffer();
+            ElementBufferObject = GL.GenBuffer();
+
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indexes.Length * sizeof(uint), indexes, BufferUsageHint.StaticDraw);
+
+            GL.BufferData(BufferTarget.ArrayBuffer, vertexs.Length * sizeof(float), vertexs, BufferUsageHint.StaticDraw);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+        }
+
+        public void Draw()
+        {
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawElements(PrimitiveType.Triangles, indexes.Length, DrawElementsType.UnsignedInt, 0);
+        }
+
+        public void Dispose()
+        {
+            GL.DeleteVertexArray(VertexArrayObject);
+            GL.DeleteBuffer(VertexBufferObject);
+            GL.DeleteBuffer(ElementBufferObject);
+        }
+    }
+}
--- Games/Pong/Pong/RenderSystem.cs
+++ Games/Pong/Pong/RenderSystem.cs
@@ -1,19 +1,12 @@
 ﻿using OpenTK.Graphics.OpenGL4;
 using System.Drawing;
 using OpenTK.Mathematics;
-using OpenTK.Windowing.Common;
-using OpenTK.Windowing.Desktop;
-using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace Pong
 {
     internal class RenderSystem : IDisposable
     {
-        private int VertexArrayObject;
-
-        private int VertexBufferObject;
-
-        private int ElementBufferObject;
+        Mesh mesh;
 
         Shader shader;
         int modelLocation;
@@ -24,10 +17,6 @@ namespace Pong
 
         public void Initialize()
         {
-            VertexArrayObject = GL.GenVertexArray();
-            VertexBufferObject = GL.GenBuffer();
-            ElementBufferObject = GL.GenBuffer();
-
             shader = new Shader("Resources/Shaders/shader.vert", "Resources/Shaders/shader.frag");
 
             modelLocation = GL.GetUniformLocation(shader.Handle, "ModelMatrix");
@@ -39,47 +28,16 @@ namespace Pong
         {
             Matrix4 model = Matrix4.CreateScale(scale.X, scale.Y, 1) * Matrix4.CreateTranslation(position.X, position.Y, 0);
 
-            float[] vertexs = {
-                 0.5f,  0.5f, 0.0f,  // top right
-                 0.5f, -0.5f, 0.0f,  // bottom right
-                -0.5f, -0.5f, 0.0f,  // bottom left
-                -0.5f,  0.5f, 0.0f   // top left
-            };
-
-            uint[] indexes = {
-                0, 1, 3,   // first triangle
-                1, 2, 3    // second triangle
-            };
-
-            GL.BindVertexArray(VertexArrayObject);
-
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
-
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, indexes.Length * sizeof(uint), indexes, BufferUsageHint.StaticDraw);
-
-            GL.BufferData(BufferTarget.ArrayBuffer, vertexs.Length * sizeof(float), vertexs, BufferUsageHint.StaticDraw);
-
             shader.Use();
 
             GL.UniformMatrix4(modelLocation, false, ref model);
             GL.UniformMatrix4(projectionLocation, false, ref projection);
 
             GL.Uniform4(colorLocation, color);
-
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
-            GL.EnableVertexAttribArray(0);
-
-            GL.BindVertexArray(VertexArrayObject);
-            GL.DrawElements(PrimitiveType.Triangles, indexes.Length, DrawElementsType.UnsignedInt, 0);
         }
 
         public void Dispose()
         {
-            GL.DeleteVertexArray(VertexArrayObject);
-            GL.DeleteBuffer(VertexBufferObject);
-            GL.DeleteBuffer(ElementBufferObject);
-
             shader.Dispose();
         }
 
