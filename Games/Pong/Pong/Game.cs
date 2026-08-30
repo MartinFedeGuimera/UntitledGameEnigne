ca5a18513c6418ddd339baac70cd194d3c651a97
Martin Guimera <martinfedeguimera2009@gmail.com>
2026-08-29 22:52:28 -0300

    Created inital RenderSystem
    Now you can call DrawRectangle() and it will do it.

--- Games/Pong/Pong/Game.cs
+++ Games/Pong/Pong/Game.cs
@@ -1,32 +1,14 @@
 ﻿using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
-using OpenTK.Windowing.GraphicsLibraryFramework;
+using System.Drawing;
 using System.Resources;
 
 namespace Pong
 {
     public class Game : GameWindow
     {
-        float[] vertices = {
-             0.5f,  0.5f, 0.0f,  // top right
-             0.5f, -0.5f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f,  // bottom left
-            -0.5f,  0.5f, 0.0f   // top left
-        };
-
-        uint[] indices = {
-            0, 1, 3,   // first triangle
-            1, 2, 3    // second triangle
-        };
-
-        int VertexArrayObject;
-
-        int VertexBufferObject;
-
-        int ElementBufferObject;
-
-        Shader shader;
+        RenderSystem render = new RenderSystem();
 
         public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() 
             { 
@@ -39,24 +21,9 @@ namespace Pong
         {
             base.OnLoad();
 
-            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            GL.ClearColor(Color.Red);
 
-            VertexArrayObject = GL.GenVertexArray();
-            GL.BindVertexArray(VertexArrayObject);
-
-            VertexBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
-
-            ElementBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
-
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
-            GL.EnableVertexAttribArray(0);
-
-            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-
-            shader = new Shader("Resources/Shaders/shader.vert", "Resources/Shaders/shader.frag");
+            render.Initialize();
         }
 
         protected override void OnUpdateFrame(FrameEventArgs args)
@@ -72,12 +39,16 @@ namespace Pong
 
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
-            shader.Use();
-
-            GL.BindVertexArray(VertexArrayObject);
-            GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+            render.DrawSquare();
 
             SwapBuffers();
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            render.Dispose();
         }
 
         protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
---
+++ Games/Pong/Pong/RenderSystem.cs
@@ -0,0 +1,64 @@
+﻿using OpenTK.Graphics.OpenGL4;
+
+namespace Pong
+{
+    internal class RenderSystem : IDisposable
+    {
+        private int VertexArrayObject;
+
+        private int VertexBufferObject;
+
+        private int ElementBufferObject;
+
+        Shader shader;
+
+        public void Initialize()
+        {
+            VertexArrayObject = GL.GenVertexArray();
+            VertexBufferObject = GL.GenBuffer();
+            ElementBufferObject = GL.GenBuffer();
+
+            shader = new Shader("Resources/Shaders/shader.vert", "Resources/Shaders/shader.frag");
+        }
+
+        public void DrawSquare()
+        {
+            float[] vertexs = {
+                 0.5f,  0.5f, 0.0f,  // top right
+                 0.5f, -0.5f, 0.0f,  // bottom right
+                -0.5f, -0.5f, 0.0f,  // bottom left
+                -0.5f,  0.5f, 0.0f   // top left
+            };
+
+            uint[] indexes = {
+                0, 1, 3,   // first triangle
+                1, 2, 3    // second triangle
+            };
+
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indexes.Length * sizeof(uint), indexes, BufferUsageHint.StaticDraw);
+
+            GL.BufferData(BufferTarget.ArrayBuffer, vertexs.Length * sizeof(float), vertexs, BufferUsageHint.StaticDraw);
+            shader.Use();
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawElements(PrimitiveType.Triangles, indexes.Length, DrawElementsType.UnsignedInt, 0);
+        }
+
+        public void Dispose()
+        {
+            GL.DeleteVertexArray(VertexArrayObject);
+            GL.DeleteBuffer(VertexBufferObject);
+            GL.DeleteBuffer(ElementBufferObject);
+
+            shader.Dispose();
+        }
+    }
+}
